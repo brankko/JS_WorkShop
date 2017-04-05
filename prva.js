a529dd0fe75423c1865626607f0fc5c014b2c249
Branko Djurkovic <branko.djurkovic@telenor.rs>
2017-04-05 15:53:16 +0200

    tipvi podataka

--- prva.js
+++ prva.js
@@ -1,1 +1,19 @@
-alert("hello world");
+var x = 5;
+var y = 12;
+var z = x + y;
+
+var ime = 'Ivana';
+var prezime = 'Ivanic';
+var punoIme = ime + ' ' + prezime;
+
+var niz = [1, 2, 3];
+
+var korisnik = {
+	ime: 'Ivana',
+	prezime: 'Ivanic',
+	punoIme: function(){
+		return this.ime + ' ' + this.prezime;
+	}
+};
+
+console.log(korisnik.punoIme());
